9d817f04c8e427f6381eedce941f24c18c2ddf62
DimaManko <mankonat@gmail.com>
2025-11-30 14:41:36 +0300

    arr task 2

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,63 @@
-// 1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
-
-// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
-
-// Имена подставляются автоматически из массива. Если массив пустой, то возвращается сообщение 'Семья пуста'
-
-// 2) напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+// 3) Задача с собеседований. Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.
 
 // Пример:
 
-// standardizeStrings(favoriteCities)  выведет в консоль
+// const someString = 'This is some strange string';
 
-// lisbon
-// rome
-// milan
-// dublin
+// reverse(someString) => 'gnirts egnarts emos si sihT'
 
-const family = ["Peter", "Ann", "Alex", "Linda"];
-function showFamily(arr) {
-  let name = "";
-  for (const element of arr) {
-    name += ` ${element}`;
+// 4) Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
+
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+// Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу (сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде.
+
+// Пример:
+
+// availableCurr(['UAH', 'RUB', 'CNY'], 'CNY')
+
+// Вернет строку:
+
+// Доступные валюты:
+// UAH
+// RUB
+
+// Заметьте:
+
+// - CNY (юань) исчез из списка валют, значит такая валюта закончилась
+
+// - После валюты: стоит перенос строки \n, и после каждой валюты тоже. Это важно для тестов
+
+// - Данные для первого аргумента должны приходить сразу из двух банков, причем сначала baseCurrencies, потом additionalCurrencies по порядку
+
+const someString = "This is some strange string";
+
+function reverse(str) {
+  if (typeof str !== "string") {
+    return "Ошибка";
   }
-  if (name === "") {
-    return "Семья пуста";
+  let arr = [];
+  for (let i = str.length; i >= 0; i--) {
+    arr.push(str[i]);
   }
-  return `Семья состоит из: ${name.trim()}`;
+  return arr.join("");
 }
 
-const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
 
-function standardizeStrings(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    console.log(element.toLowerCase());
+function availableCurr(arr, missingCurr) {
+  let string = "Доступные валюты:\n";
+  if (arr.length === 0) {
+    return "Нет доступных валют";
   }
+  for (const elem of arr) {
+    if (elem !== missingCurr) {
+      string += `${elem}\n`;
+    }
+  }
+  return string.trim();
 }
 
-console.log(showFamily(family));
-standardizeStrings(favoriteCities);
+console.log(availableCurr(["UAH", "RUB", "CNY"], "CNY"));
