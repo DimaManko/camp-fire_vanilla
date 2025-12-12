e317946f337b4f8b0ae5d746e408cac0357522bd
DimaManko <mankonat@gmail.com>
2025-12-12 23:08:32 +0300

    project

---
+++ project/js/script.js
@@ -0,0 +1,76 @@
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+"use strict";
+
+const movieDB = {
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
+};
+
+const promoGenre = document.querySelector(".promo__genre");
+const adv = document.querySelectorAll(".promo__adv img");
+const promoBg = document.querySelector(".promo__bg");
+const interactiveList = document.querySelector(".promo__interactive-list");
+const btn = document.querySelector("button");
+const inp = document.querySelector(".adding__input");
+
+promoGenre.textContent = "драма";
+
+adv.forEach((item) => {
+  item.remove();
+});
+
+promoBg.style.backgroundImage = "url('./img/bg.jpg')";
+
+function sortArr(arr) {
+  arr.sort();
+}
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let newFilm = inp.value;
+  if (newFilm) {
+    if (newFilm.length > 21) {
+      newFilm = newFilm.substring(0, 21) + "...";
+    }
+    movieDB.movies.push(newFilm);
+    sortArr(movieDB.movies);
+    interactiveList.innerHTML = "";
+    addNewFilm(movieDB.movies, interactiveList);
+  }
+});
+
+function addNewFilm(films, list) {
+  films.forEach((film, index) => {
+    const li = document.createElement("li");
+    li.classList.add("promo__interactive-item");
+    li.textContent = `${index + 1}. ${film}`;
+    list.append(li);
+    const div = document.createElement("div");
+    div.classList.add("delete");
+    li.append(div);
+  });
+}
+
+addNewFilm(movieDB.movies, interactiveList);
