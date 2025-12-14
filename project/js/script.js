af18869341bf51338202ba462e3d39ceeb70cfd7
DimaManko <mankonat@gmail.com>
2025-12-14 13:27:56 +0300

    dynamic create list on DB

--- project/js/script.js
+++ project/js/script.js
@@ -17,60 +17,81 @@ P.S. Здесь есть несколько вариантов решения з
 
 "use strict";
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
 
-const promoGenre = document.querySelector(".promo__genre");
-const adv = document.querySelectorAll(".promo__adv img");
-const promoBg = document.querySelector(".promo__bg");
-const interactiveList = document.querySelector(".promo__interactive-list");
-const btn = document.querySelector("button");
-const inp = document.querySelector(".adding__input");
+  const promoGenre = document.querySelector(".promo__genre");
+  const adv = document.querySelectorAll(".promo__adv img");
+  const promoBg = document.querySelector(".promo__bg");
+  const interactiveList = document.querySelector(".promo__interactive-list");
+  const btn = document.querySelector("button");
+  const inp = document.querySelector(".adding__input");
 
-promoGenre.textContent = "драма";
+  const deleteAdv = (adv) => {
+    adv.forEach((item) => {
+      item.remove();
+    });
+  };
 
-adv.forEach((item) => {
-  item.remove();
-});
+  const changes = () => {
+    promoBg.style.backgroundImage = "url('./img/bg.jpg')";
+    promoGenre.textContent = "драма";
+  };
 
-promoBg.style.backgroundImage = "url('./img/bg.jpg')";
-
-function sortArr(arr) {
-  arr.sort();
-}
-
-btn.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  let newFilm = inp.value;
-  if (newFilm) {
-    if (newFilm.length > 21) {
-      newFilm = newFilm.substring(0, 21) + "...";
-    }
-    movieDB.movies.push(newFilm);
-    sortArr(movieDB.movies);
-    interactiveList.innerHTML = "";
-    addNewFilm(movieDB.movies, interactiveList);
+  function sortArr(arr) {
+    arr.sort();
   }
-});
 
-function addNewFilm(films, list) {
-  films.forEach((film, index) => {
-    const li = document.createElement("li");
-    li.classList.add("promo__interactive-item");
-    li.textContent = `${index + 1}. ${film}`;
-    list.append(li);
-    const div = document.createElement("div");
-    div.classList.add("delete");
-    li.append(div);
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const checkbox = document.querySelector('[type="checkbox"]');
+
+    let newFilm = inp.value;
+    if (newFilm) {
+      if (newFilm.length > 21) {
+        newFilm = newFilm.substring(0, 22) + "...";
+      }
+      if (checkbox.checked) {
+        console.log("Добавляем любимый фильм");
+      }
+
+      movieDB.movies.push(newFilm);
+      sortArr(movieDB.movies);
+      addNewFilm(movieDB.movies, interactiveList);
+    }
   });
-}
 
-addNewFilm(movieDB.movies, interactiveList);
+  function addNewFilm(films, list) {
+    interactiveList.innerHTML = "";
+    sortArr(movieDB.movies);
+    films.forEach((film, index) => {
+      const li = document.createElement("li");
+      li.classList.add("promo__interactive-item");
+      li.textContent = `${index + 1}. ${film}`;
+      list.append(li);
+      const div = document.createElement("div");
+      div.classList.add("delete");
+      li.append(div);
+    });
+
+    document.querySelectorAll(".delete").forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+        movieDB.movies.splice(i, 1);
+        addNewFilm(films, list);
+      });
+    });
+  }
+
+  addNewFilm(movieDB.movies, interactiveList);
+  deleteAdv(adv);
+  changes();
+});
