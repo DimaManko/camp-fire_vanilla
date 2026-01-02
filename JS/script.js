67c6027803bad7834bc39dc41ef9f88e17a87620
DimaManko <mankonat@gmail.com>
2026-01-02 16:39:25 +0300

    destructurisation

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,15 @@
-// Парсинг JSON и reviver
-// На вход дается JSON-строка с объектом внутри, необходимо самостоятельно получить ее и распарсить.
+// Вам в переменной author предоставлен объект, необходимо с помощью деструктуризации достать два значения из этого объекта:
 
-// Нас будет интересовать свойство date в объекте, оно хранит дату и время.
-
-// Вам необходимо с помощью reviver (второго аргумента функции JSON.parse) преобразовать строку в объект Date и вывести день месяца, который получится, если к свойству date прибавить 15 дней.
+// Имя автора - свойство firstname, которое необходимо положить в константу authorName.
+// У объекта в свойстве books лежит массив объектов - книги автора. Необходимо у первого объекта в массиве достать свойство title, это значение должно находиться в константе firstBook.
 function sumDate(data) {
-  const date = JSON.parse(data, (key, value) => {
-    if (key === "date") {
-      const objDate = new Date(value);
-      const newDay = objDate.getDate() + 15;
-      const newDayObj = new Date(objDate.setDate(newDay));
-      console.log(newDayObj.getDate());
-    }
-  });
+  const {
+    firstname: authorName,
+    books: [{ title: firstBook }],
+  } = JSON.parse(data.toString());
+  console.log(`${authorName} - ${firstBook}`);
 }
 
-sumDate('{"date":"2027-11-27T10:10:10Z"}');
+sumDate(
+  '{"firstname": "Александр", "books": [{"title": "Евгений Онегин", "form": "роман в стихах"}, {"title": "Капитанская дочка"}]}'
+);
