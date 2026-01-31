ee949d18dc32edb8fbc0da14841dfa20051c9b3f
DimaManko <mankonat@gmail.com>
2026-01-31 16:36:46 +0300

    filter and reduce for arr

--- JS/script.js
+++ JS/script.js
@@ -1,59 +1,36 @@
-// 1) У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+"use strict";
 
-// 2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
-
-// Пример:
-
-// showListOfFilms(films) => "Titanic, Die hard 5, Matrix, Some bad film"
-
-// 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+// 1) У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
 
 // Пример:
 
-// setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
+// getPositiveIncomeAmount(funds) => 13300
 
-// 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); :)
+// 2) Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов содержит отрицательное значение поля amount, то функция возвращает сумму всех значений. (число) Если таких значений нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
 
-const films = [
-  {
-    name: "Titanic",
-    rating: 9,
-  },
-  {
-    name: "Die hard 5",
-    rating: 5,
-  },
-  {
-    name: "Matrix",
-    rating: 8,
-  },
-  {
-    name: "Some bad film",
-    rating: 4,
-  },
+// Пример:
+
+// getTotalIncomeAmount(funds) => -500
+
+const funds = [
+  { amount: -1400 },
+  { amount: 2400 },
+  { amount: -1000 },
+  { amount: 500 },
+  { amount: 10400 },
+  { amount: -11400 },
 ];
 
-function showGoodFilms(arr) {
-  return arr.filter((film) => film.rating >= 8);
-}
+const getPositiveIncomeAmount = (data) => {
+  return data
+    .filter((obj) => obj.amount > 0)
+    .reduce((sum, obj) => sum + obj.amount, 0);
+};
 
-function showListOfFilms(arr) {
-  return arr.reduce(
-    (acc, curr) => `${typeof acc === "object" ? acc.name : acc}, ${curr.name}`,
-  );
-}
+const getTotalIncomeAmount = (data) => {
+  return data.some((obj) => obj.amount < 0)
+    ? data.reduce((sum, obj) => sum + obj.amount, 0)
+    : getPositiveIncomeAmount(data);
+};
 
-function setFilmsIds(arr) {
-  return arr.map((film, index) => {
-    film.id = index;
-    return film;
-  });
-}
-
-const tranformedArray = setFilmsIds(films);
-
-function checkFilms(arr) {
-  return arr.every((film) => film.id || film.id === 0);
-}
-
-console.log(showListOfFilms(films));
+console.log(getTotalIncomeAmount(funds));
