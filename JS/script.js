5f31c75128a01ea33723a66cee1360afe0eeed39
DimaManko <mankonat@gmail.com>
2026-02-07 19:58:11 +0300

    sort personeNlo whis methid entries()

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,36 @@
-// Анализируем логи
-// Вы работаете с логами сервера. Каждый лог представляет собой объект с двумя полями:
+// Вам на вход подается массив объектов, представляющих инопланетян, каждый из которых имеет имя и уровень IQ.
 
-// endpoint — строка, путь к API-эндпоинту.
-// status — целое число, HTTP-статус ответа.
-// Вам подается массив таких логов, необходимо вывести в консоль объект в формате JSON, в котором по каждому endpoint подсчитано количество успешных и неуспешных запросов.
+// Ваша задача - отсортировать список по убыванию IQ инопланетян и вывести его с их порядковым номером и значением IQ, используя метод массива .entries() в цикле for.
 
-// Успешным запросом является любой статус в диапазоне 200–299, а неуспешным — любой другой статус (например, 4xx, 5xx и т.п.).
+// Используйте следующий формат вывода:
 
-// Подсказка:
-// Как отмечалось, формат вывода - JSON, можно использовать JSON.stringify(), однако, для правильного формата необходимо будет этой функции дать дополнительные аргументы, помимо самого объекта, который необходимо вывести.
+// [номер]: [имя] (IQ: [iq])
+
+// Sample Input 1:
+
+// [
+//   { "name": "Капитан Керн", "iq": 150 },
+//   { "name": "Глорибас", "iq": 120 },
+//   { "name": "Танос", "iq": 200 }
+// ]
+// Sample Output 1:
+
+// 1: Танос (IQ: 200)
+// 2: Капитан Керн (IQ: 150)
+// 3: Глорибас (IQ: 120)
 
 const data = `[
-  { "endpoint": "/api/cart", "status": 200 },
-  { "endpoint": "/api/cart", "status": 403 },
-  { "endpoint": "/api/checkout", "status": 502 },
-  { "endpoint": "/api/checkout", "status": 200 },
-  { "endpoint": "/api/cart", "status": 200 }
+  { "name": "Капитан Керн", "iq": 150 },
+  { "name": "Глорибас", "iq": 120 },
+  { "name": "Танос", "iq": 200 }
 ]`;
 
-function checkLog(data) {
-  const log = JSON.parse(data);
-  const check = log.reduce((acc, point) => {
-    if (!acc[point.endpoint]) {
-      acc[point.endpoint] = { success: 0, error: 0 };
-    }
-    point.status >= 200 && point.status < 300
-      ? (acc[point.endpoint].success += 1)
-      : (acc[point.endpoint].error += 1);
-    return acc;
-  }, {});
-  console.log(JSON.stringify(check));
+function sortIqNlo(data) {
+  const nlo = JSON.parse(data);
+  const sortNlo = nlo.sort((a, b) => b.iq - a.iq);
+  for (const [idx, persone] of sortNlo.entries()) {
+    console.log(`${idx + 1}. ${persone.name} (IQ: ${persone.iq})`);
+  }
 }
 
-checkLog(data);
+sortIqNlo(data);
