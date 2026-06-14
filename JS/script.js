8ca20cb7734ecb6363d8b51982120024baf2f8cc
DimaManko <mankonat@gmail.com>
2026-06-14 15:45:11 +0300

    login, update API task

--- JS/script.js
+++ JS/script.js
@@ -1,107 +1,54 @@
-async function getCategoryPriceAnalysis() {
-  const _baseURL = new URL(`https://clothapi.progskill.ru/v1/categories`);
-  try {
-    const categoriesResponse = await fetch(_baseURL);
-    if (!categoriesResponse.ok) {
-      throw new Error(`Неверный статус запроса: ${categoriesResponse.status}`);
-    }
-    const categories = await categoriesResponse.json();
-    const categoriesPromises = categories.data.map(async (category) => {
-      const url = new URL("https://clothapi.progskill.ru/v1/products");
-      url.searchParams.append("category_id", category.id);
-      url.searchParams.append("min_stock", "1");
-      url.searchParams.append("order_by", "price_asc");
-      url.searchParams.append("page", "1");
-
-      const responseProd = await fetch(url);
-      if (!responseProd.ok) {
-        throw new Error(`Неверный статус запроса: ${responseProd.status}`);
-      }
-      const data = await responseProd.json();
-      const arrProd = [...data.data];
-      const totalPage = data.total_pages || 1;
-      const totalProd = data.total_count;
-      const countAnalyze = Math.ceil(totalProd * 0.1);
-
-      if (totalProd === 0) {
-        console.log(`Товары ${category.name} отсутсвует в наличии`);
-        return {
-          categoriesName: category.name,
-          productsAnalyze: [],
-        };
-      }
-
-      if (totalPage > 1 && countAnalyze > arrProd.length) {
-        const pagePromises = [];
-        const pageNeeded = Math.ceil(countAnalyze / data.data.length);
-        console.log(pageNeeded);
-
-        for (let page = 2; page <= pageNeeded; page++) {
-          const url = new URL("https://clothapi.progskill.ru/v1/products");
-          url.searchParams.append("category_id", category.id);
-          url.searchParams.append("min_stock", "1");
-          url.searchParams.append("order_by", "price_asc");
-          url.searchParams.append("page", `${page}`);
-
-          const req = fetch(url);
-          pagePromises.push(req);
-        }
-        const pageResponse = await Promise.all(pagePromises);
-        const pageData = await Promise.all(
-          pageResponse.map((res) => {
-            if (!res.ok) {
-              throw new Error(`Ошибка загрузки страницы`);
-            }
-            return res.json();
-          }),
-        );
-        pageData.forEach((pageResult) => {
-          arrProd.push(...pageResult.data);
+function login(username, pass) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://clothapi.progskill.ru/v1/auth/login");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.addEventListener("load", () => {
+      if (request.status === 200) {
+        const responseData = JSON.parse(request.responseText);
+        resolve({
+          user: responseData.user,
+          accessToken: responseData.access_token.value,
         });
+      } else if (request.status === 401) {
+        reject(new Error("Ошибка 401: Неверный логин или пароль."));
+      } else {
+        reject(new Error(`Ошибка: Не удалось отправить данные.`));
       }
-
-      const finalArrProductsAnalyze = arrProd.slice(0, countAnalyze);
-
-      return {
-        categoriesName: category.name,
-        productsAnalyze: finalArrProductsAnalyze,
-      };
     });
-    const productsByCategories = await Promise.all(categoriesPromises);
-    console.log(productsByCategories);
-    const dataAnalyze = new Map();
-    productsByCategories.forEach((product) => {
-      if (product.productsAnalyze.length === 0) {
-        return;
-      }
-      const totalPriceCategories = product.productsAnalyze.reduce(
-        (acc, item) => acc + item.price,
-        0,
-      );
-      const averagePrice = Math.round(
-        totalPriceCategories / product.productsAnalyze.length,
-      );
-      console.log(averagePrice);
-      dataAnalyze.set(product.categoriesName, averagePrice);
+    request.addEventListener("error", () => {
+      reject(new Error(`Сетевая ошибка запроса`));
     });
-    console.log(dataAnalyze);
-
-    return dataAnalyze;
-  } catch (error) {
-    return Promise.reject(error);
-  }
+    request.send(JSON.stringify({ login: username, password: pass }));
+  });
 }
-getCategoryPriceAnalysis()
-  .then((priceMap) => {
-    if (priceMap.size === 0) {
-      console.log("Нет данных для анализа.");
-    } else {
-      console.log("Результаты анализа (10-й перцентиль):");
-      for (const [categoryName, avgPrice] of priceMap) {
-        console.log(
-          `Категория: ${categoryName}, Средняя цена (10% дешёвых): ${avgPrice}`,
-        );
+
+function updateFirstName(accessToken, userId, newUserName) {
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open("PATCH", `https://clothapi.progskill.ru/v1/users/${userId}`);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("Authorization", `Bearer ${accessToken}`);
+    req.addEventListener("load", () => {
+      if (req.status === 200) {
+        const responseData = JSON.parse(req.responseText);
+        resolve(responseData);
+      } else if (req.status === 401) {
+        reject(new Error(`Ошибка 401: Токен устарел.`));
+      } else {
+        reject(new Error(`Ошибка: Не удалось отправить данные.`));
       }
-    }
-  })
-  .catch((error) => console.error("Ошибка анализа:", error));
+    });
+    req.addEventListener("error", () => {
+      reject(new Error(`Сетевая ошибка запроса`));
+    });
+    req.send(JSON.stringify({ first_name: newUserName }));
+  });
+}
+
+login("admin", "admin")
+  .then((data) => updateFirstName(data.accessToken, data.user.id, "asdasd"))
+  .then((user) =>
+    console.log(`Новое полное имя: ${user.first_name} ${user.last_name}`),
+  )
+  .catch((error) => console.error(error));
