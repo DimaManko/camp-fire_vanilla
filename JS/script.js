fac33384324ca55318a6c7d840c19f86aa19bf9e
DimaManko <mankonat@gmail.com>
2026-06-14 22:35:09 +0300

    login and increaseCheapestProductPrice task fetch API

--- JS/script.js
+++ JS/script.js
@@ -1,54 +1,88 @@
-function login(username, pass) {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://clothapi.progskill.ru/v1/auth/login");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.addEventListener("load", () => {
-      if (request.status === 200) {
-        const responseData = JSON.parse(request.responseText);
-        resolve({
-          user: responseData.user,
-          accessToken: responseData.access_token.value,
-        });
-      } else if (request.status === 401) {
-        reject(new Error("Ошибка 401: Неверный логин или пароль."));
-      } else {
-        reject(new Error(`Ошибка: Не удалось отправить данные.`));
-      }
+async function login(username, pass) {
+  try {
+    const url = new URL(`https://clothapi.progskill.ru/v1/auth/login`);
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ login: username, password: pass }),
+      credentials: "include",
     });
-    request.addEventListener("error", () => {
-      reject(new Error(`Сетевая ошибка запроса`));
-    });
-    request.send(JSON.stringify({ login: username, password: pass }));
-  });
+    if (!res.ok) {
+      throw new Error(`Неверный статус ответа: ${res.status}`);
+    }
+    const data = await res.json();
+    return data.access_token.value;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
-function updateFirstName(accessToken, userId, newUserName) {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open("PATCH", `https://clothapi.progskill.ru/v1/users/${userId}`);
-    req.setRequestHeader("Content-Type", "application/json");
-    req.setRequestHeader("Authorization", `Bearer ${accessToken}`);
-    req.addEventListener("load", () => {
-      if (req.status === 200) {
-        const responseData = JSON.parse(req.responseText);
-        resolve(responseData);
-      } else if (req.status === 401) {
-        reject(new Error(`Ошибка 401: Токен устарел.`));
-      } else {
-        reject(new Error(`Ошибка: Не удалось отправить данные.`));
-      }
-    });
-    req.addEventListener("error", () => {
-      reject(new Error(`Сетевая ошибка запроса`));
-    });
-    req.send(JSON.stringify({ first_name: newUserName }));
-  });
+async function increaseCheapestProductPrice(accessToken, brendId, procent) {
+  try {
+    const url = new URL(`https://clothapi.progskill.ru/v1/products`);
+    url.searchParams.append("brand_id", `${brendId}`);
+    url.searchParams.append("order_by", "price_asc");
+    url.searchParams.append("page", "1");
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Неверный статус ответа: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.data.length === 0 || !data.data) {
+      throw new Error("На данный момент товары у выбранного бренда отсутсвуют");
+    }
+    const lowestPriceProduct = data.data[0];
+    console.log(lowestPriceProduct);
+
+    const newPrice = Math.round(lowestPriceProduct.price * (1 + procent / 100));
+
+    const updateResponse = await fetch(
+      `https://clothapi.progskill.ru/v1/products/${lowestPriceProduct.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          category_id: lowestPriceProduct.category_id,
+          brand_id: lowestPriceProduct.brand_id,
+          name: lowestPriceProduct.name,
+          description: lowestPriceProduct.description,
+          price: newPrice,
+          stock: lowestPriceProduct.stock,
+        }),
+      },
+    );
+    if (!updateResponse.ok) {
+      throw new Error(
+        `Не удалось обновить цену: статус ${updateResponse.status}`,
+      );
+    }
+    const updateData = await updateResponse.json();
+    return updateData;
+  } catch (e) {
+    throw e;
+  }
 }
 
 login("admin", "admin")
-  .then((data) => updateFirstName(data.accessToken, data.user.id, "asdasd"))
-  .then((user) =>
-    console.log(`Новое полное имя: ${user.first_name} ${user.last_name}`),
+  .then((token) =>
+    increaseCheapestProductPrice(
+      token,
+      "b3d4eef6-9e02-4858-b692-0000596f3a2d",
+      15.5,
+    ),
   )
-  .catch((error) => console.error(error));
+  .then((updatedProduct) => {
+    console.log(
+      `Цена товара "${updatedProduct.name}" обновлена, новая цена: ${updatedProduct.price}`,
+    );
+  })
+  .catch((error) => console.error("Ошибка:", error));
